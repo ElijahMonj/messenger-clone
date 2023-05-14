fd58f88d4d082c7bf23e0ed49bd5c63387a444cd
Elijah Monjardin <monjardinelijah120@gmail.com>
2023-05-14 17:23:24 +0800

    Added login functionality with image and sidebar

--- app/components/sidebar/DesktopSidebar.tsx
+++ app/components/sidebar/DesktopSidebar.tsx
@@ -1,15 +1,21 @@
 'use client'
 
 import useRoutes from "@/app/hooks/useRoutes";
+import { User } from "@prisma/client";
 import { useState } from "react";
 import { icons } from "react-icons";
+import Avatar from "../Avatar";
 import DesktopItem from "./DesktopItem";
 
-
-const DesktopSidebar = () => {
+interface DesktopSidebarProps{
+    currentUser: User
+}
+const DesktopSidebar: React.FC<DesktopSidebarProps> = ({
+    currentUser
+}) => {
     const routes= useRoutes();
     const [isOpen, setIsOpen]=useState(false);
-
+    console.log({currentUser})
 
     return ( 
         <div className="
@@ -48,7 +54,19 @@ const DesktopSidebar = () => {
                     ))}
                 </ul>
             </nav>  
-
+            <nav className="
+                mt-4
+                flex
+                flex-col
+                justify-between
+                items-center
+            ">
+                <div onClick={()=> setIsOpen(true)}
+                    className='cursor-pointer hover:opacity-75 transition'
+                >
+                    <Avatar user={currentUser}/>
+                </div>
+            </nav>
         </div>
      );
 }
